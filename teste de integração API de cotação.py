17e7dbbf2b2b694303d7c2f76a822cb1fd5c9846
Fernanda assunção <fernandaassuncaosa@gmail.com>
2026-04-02 14:56:25 -0300

    Terminando o teste do sistema de cotação

--- teste de integração API de cotação.py
+++ teste de integração API de cotação.py
@@ -17,8 +17,53 @@ class SistemaDeCotacao:
             lista = [item, float(dic[item]['bid'])]
             self.moedas_disponiveis.append(item)
             self.moedas.append(copy.deepcopy(lista))
-        print(self.moedas)
-        print(self.moedas_disponiveis)
+
+    def ver_moedas(self):
+        texto = 'As moedas disponiveis são: '
+        for moedas in self.moedas_disponiveis:
+            texto += f'\033[1:35m{moedas}, '
+        print(f'{texto}\033[m')
+        print('=' * 30)
+
+    def escolher_moeda(self):
+        try:
+            escolha = str(input('Digite sua escolha: ')).strip().upper()
+            if escolha in self.moedas_disponiveis:
+                self.moeda = escolha
+                print(f'\033[1:32mMoeda escolhida com sucesso!!{self.moeda}\033[m')
+            else:
+                raise
+        except:
+            print('\033[1:31mEscolha invalida!\033[m')
+        print('=' * 30)
+
+    def ver_cotacao(self):
+        try:
+            if self.moeda is None:
+                raise
+            else:
+                for moeda in self.moedas:
+                    if moeda[0] == self.moeda:
+                        print(f'\033[1:36mA cotação da {moeda[0]} é {moeda[1]:.2f}\033[m')
+        except:
+            print('\033[1:31mERRO, escolha uma moeda primeiro!!\033[m')
+        print('=' * 30)
 
 
 cotacao = SistemaDeCotacao()
+while True:
+    print('''\033[1:34mSISTEMA DE COTAÇÃO
+    [1] Ver moedas disponiveis
+    [2] Escolher moeda
+    [3] Ver cotação
+    [4] Sair\033[m''')
+    opcao = int(input('Digite sua opção: '))
+    print('=' * 30)
+    if opcao == 1:
+        cotacao.ver_moedas()
+    elif opcao == 2:
+        cotacao.escolher_moeda()
+    elif opcao == 3:
+        cotacao.ver_cotacao()
+    elif opcao == 4:
+        break
